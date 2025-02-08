022ea4611e26c7becb91e7baf26cb88a481d0920
sabbirz <6649698+sabbirkuasha@users.noreply.github.com>
2025-02-08 13:32:26 +0600

    doct: Clinic DocType Created

---
+++ doctapp/doctapp/doctype/doctor/doctor.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, sabbir and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Doctor", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ doctapp/doctapp/doctype/doctor/doctor.py
@@ -0,0 +1,41 @@
+# Copyright (c) 2025, sabbir and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Doctor(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		first_name: DF.Data
+		full_name: DF.Data | None
+		last_name: DF.Data | None
+		name: DF.Int | None
+		qualification: DF.Data | None
+		speciality: DF.Data
+	# end: auto-generated types
+
+	def validate(self):
+		self.set_full_name()
+
+	def set_full_name(self):
+		"""
+        Sets the full name of the doctor based on first and last names.
+        Handles cases where the last name might be missing.
+        """
+		# Plain Simple Logic
+		# self.full_name = self.first_name + ' ' + self.last_name
+		
+		# Handle if last name not present
+		self.full_name = (
+			(self.first_name + ' ' + self.last_name) if self.last_name else self.first_name
+			)
+	
+
